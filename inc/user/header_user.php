fc81b18621d178a9d87dad55463d4c689d75b28a
Arbani <159100563+Girinko@users.noreply.github.com>
2025-12-19 14:19:27 +0700

    ui/user

--- inc/user/header_user.php
+++ inc/user/header_user.php
@@ -7,8 +7,8 @@ include "function/function_user.php";
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Bootstrap CRUD Data Table for Database with Modal Form</title>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+<title>Surat Warga</title>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700">
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -18,26 +18,24 @@ include "function/function_user.php";
 
 </head>
 <body>
-    <div class="container">
-		<div class="table-responsive">
-			<div class="table-wrapper">
-				<div class="table-title">
-					<div class="row">
-						<div class="col-xs-6">
-							<h2><b>Surat Warga</b></h2>
-						</div>
-						<div class="col-xs-6">
-							<a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add New Employee</span></a>				
-						</div>
-					</div>
-				</div>
-				<table class="table table-striped table-hover">
-					<thead>
-						<tr>
+    <div class="main-container">
+        <div class="header-section">
+            <h2>Surat Warga</h2>
+            <button class="btn-add" data-toggle="modal" data-target="#addEmployeeModal">
+                <i class="material-icons">add</i>
+                <span>Tambah Surat</span>
+            </button>
+        </div>
+
+        <div class="table-container">
+            <div class="table-wrapper">
+                <table class="data-table">
+                    <thead>
+                        <tr>
                             <th>ID</th>
-							<th>Nama</th>
-							<th>Surat</th>
-							<th>Alamat</th>
-							<th>Status</th>
-						</tr>
-					</thead>
+                            <th>Nama</th>
+                            <th>Surat</th>
+                            <th>Alamat</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
--- index.php
+++ index.php
@@ -11,75 +11,78 @@ if (isset($_POST['submit']) == TRUE) {
     $get->add_data($nama, $surat, $alamat);
 }
 ?>
-					<tbody>
-                        <?php foreach ($get->get_data() as $row) : ?>
+
+                    <tbody>
+                        <?php 
+                        $data = $get->get_data();
+                        if (count($data) > 0):
+                            foreach ($data as $row) : 
+                        ?>
                             <tr>
                                 <td><?php echo $row['id']; ?></td>
                                 <td><?php echo $row['nama']; ?></td>
                                 <td><?php echo $row['surat']; ?></td>
                                 <td><?php echo $row['alamat']; ?></td>
-                                <td><?php echo $row['status']; ?></td>
-                                <!-- <td>
-								<a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-								<a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-							    </td> -->
+                                <td>
+                                    <span class="status-badge <?php 
+                                        if ($row['status'] == 'Pending') echo 'status-pending';
+                                        elseif ($row['status'] == 'Approved') echo 'status-approved';
+                                        elseif ($row['status'] == 'Rejected') echo 'status-rejected';
+                                        else echo 'status-pending';
+                                    ?>">
+                                        <?php echo $row['status']; ?>
+                                    </span>
+                                </td>
                             </tr>
-                        <?php endforeach; ?>
-					</tbody>
-				</table>    
-
-                            
-		</div>        
+                        <?php 
+                            endforeach;
+                        else:
+                        ?>
+                            <tr>
+                                <td colspan="5">
+                                    <div class="empty-state">
+                                        <i class="material-icons">inbox</i>
+                                        <p>No data available</p>
+                                    </div>
+                                </td>
+                            </tr>
+                        <?php endif; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
-	<div id="addEmployeeModal" class="modal fade">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<form action="index.php" method="post">
-					<div class="modal-header">						
-						<h4 class="modal-title">Add Employee</h4>
-						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-					</div>
-					<div class="modal-body">					
-						<div class="form-group">
-							<label>Nama</label>
-							<input type="text" class="form-control" required name="nama">
-						</div>
-						<div class="form-group">
-							<label>Surat</label>
-							<input type="text" class="form-control" required name="surat">
-						</div>
-						<div class="form-group">
-							<label>Alamat</label>
-							<textarea type="text" class="form-control" required name="alamat"></textarea>
-						</div>			
-					</div>
-					<div class="modal-footer">
-						<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-						<input type="submit" class="btn btn-success" value="Add" name="submit">
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-	<!-- <div id="deleteEmployeeModal" class="modal fade">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<form>
-					<div class="modal-header">						
-						<h4 class="modal-title">Delete Employee</h4>
-						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-					</div>
-					<div class="modal-body">					
-						<p>Are you sure you want to delete these Records?</p>
-						<p class="text-warning"><small>This action cannot be undone.</small></p>
-					</div>
-					<div class="modal-footer">
-						<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-						<input type="submit" class="btn btn-danger" value="Delete">
-					</div>
-				</form>
-			</div>
-		</div>
-	</div> -->
+
+    <!-- Add Modal -->
+    <div id="addEmployeeModal" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form action="index.php" method="post">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Add New Data</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label>Nama</label>
+                            <input type="text" class="form-control" required name="nama" placeholder="Enter name">
+                        </div>
+                        <div class="form-group">
+                            <label>Surat</label>
+                            <input type="text" class="form-control" required name="surat" placeholder="Enter document type">
+                        </div>
+                        <div class="form-group">
+                            <label>Alamat</label>
+                            <textarea class="form-control" required name="alamat" placeholder="Enter address"></textarea>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn-cancel" data-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn-submit" name="submit">Add Data</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
